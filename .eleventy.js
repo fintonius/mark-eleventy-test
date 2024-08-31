ca05909affee692bbdd763e06ea32a82aa561b0d
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-08-31 20:10:31 +0100

    update eleventy config to send admin folder when building site

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/home-page-style.css');
     eleventyConfig.addPassthroughCopy('src/project-style.css');
     eleventyConfig.addPassthroughCopy('src/assets');
+    eleventyConfig.addPassthroughCopy('src/admin');
     eleventyConfig.addPassthroughCopy('src/script.js');
     
     return {
