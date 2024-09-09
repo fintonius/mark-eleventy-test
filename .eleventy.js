7b5d0a1c1083ffaeedac032b3de7d73b026b2f9f
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-09-09 11:14:07 +0100

    configure custom markdown filter for eleventy to use

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
-module.exports = function(eleventyConfig) {
+import markdownIt from "markdown-it";
+
+export default function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('src/style.css');
     eleventyConfig.addPassthroughCopy('src/home-page-style.css');
@@ -6,6 +8,19 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addPassthroughCopy('src/admin');
     eleventyConfig.addPassthroughCopy('src/script.js');
+    
+    // create a markdown filter to convert MD to HTML
+    const md = markdownIt({
+        html: true,
+    });
+
+    // adds the filter 'markdown' to eleventy/Nunjucks
+    eleventyConfig.addNunjucksFilter("markdown", (content) => {
+        return md.render(content);
+    });
+
+    // configures Eleventy to use md as the specific instance of markdown-it
+    eleventyConfig.setLibrary("md", md);
     
     return {
         dir: {
